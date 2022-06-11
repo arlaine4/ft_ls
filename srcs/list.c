16062fad4dd112f11500d09386cdba3ef97e8ca1
Arthur LAINE <arlaine@e2r2p6.clusters.42paris.fr>
2022-06-11 16:21:38 +0200

    nice dereference of linked list, added a function to add a new node to the linked list and one to fill it's content with dereference

--- srcs/list.c
+++ srcs/list.c
@@ -1,13 +1,35 @@
 # include "../includes/header.h"
 
+void	fill_list_content(t_file **node, struct dirent *content)
+{
+	(*node)->name = content->d_name;
+	(*node)->type = content->d_type;
+	(*node)->next = NULL;
+}
+
 t_file	*init_list_head(struct dirent *entry)
 {
 	t_file	*head;
 	head = (t_file *)malloc(sizeof(t_file));
 	if (head == NULL)
 		return NULL;
-	head->name = entry->d_name;
-	head->type = entry->d_type;
-	head->next = NULL; 
+	fill_list_content(&head, entry);
+	//head->name = entry->d_name;
+	//head->type = entry->d_type;
+	//head->next = NULL; 
 	return head;
+}
+
+t_file	*add_node_to_list(t_file **prev, struct dirent *content)
+{
+	t_file	*node;
+	node = (t_file *)malloc(sizeof(t_file));
+	if (node == NULL)
+		return NULL;
+	(*prev)->next = node;
+	fill_list_content(&node, content);
+	//node->name = entry->d_name;
+	//node->type = entry->d_type;
+	//node->next = NULL;
+	return node;
 }
